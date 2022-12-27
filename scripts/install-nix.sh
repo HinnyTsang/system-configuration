d29aa6f8b176b92ddbc436a315b33ae2a2b6d915
hinnytsang <mhtsangab@connect.ust.hk>
2022-12-27 12:53:22 +0800

    Update setting for wsl

--- scripts/install-nix.sh
+++ scripts/install-nix.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Install Nix
-sh <(curl -L https://nixos.org/nix/install) --darwin-use-unencrypted-nix-store-volume --daemon
+# sh <(curl -L https://nixos.org/nix/install) --darwin-use-unencrypted-nix-store-volume --daemon # installation with macOS
+sh <(curl -L https://nixos.org/nix/install) --no-daemon # install with wsl, single user mode.
 
 # Add channel for home manager.
 nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz home-manager
@@ -9,7 +10,7 @@ nix-channel --update
 
 # Export NIX_PATH
 export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
-# echo 'export NIX_PATH=${NIX_PATH:+$NIX_PATH:}$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels\n' >> ~/.bash_profile
+echo 'export NIX_PATH=${NIX_PATH:+$NIX_PATH:}$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels\n' >> ~/.bash_profile
 # echo 'export NIX_PATH=${NIX_PATH:+$NIX_PATH:}$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels\n' >> ~/.zshenv 
 
 # Install home manager
@@ -19,7 +20,7 @@ nix-shell '<home-manager>' -A install
 . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
 
 # Override the default config with our config.
-cp ./home.nix ~/.config/nixpkgs/home.nix
+cp *.nix ~/.config/nixpkgs/
 
 # build the home environment
 home-manager build
