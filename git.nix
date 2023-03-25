c71f55366635d952cde87cde73285a8e3091418d
hinnytsang <mhtsangab@connect.ust.hk>
2023-03-26 02:34:49 +0800

    Update git alias

--- git.nix
+++ git.nix
@@ -5,5 +5,10 @@
     enable = true;
     userEmail = "mhstsangab@connect.ust.hk";
     userName = "HinnyTsang";
+    aliases = {
+      l =
+        "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
+      tree = "ls-tree -r --name-only HEAD | tree --fromfile";
+    };
   };
 }
