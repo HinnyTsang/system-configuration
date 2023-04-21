da2d9b8ad26c39b624960bfd09155043a7d80ecb
hinnytsang <mhtsangab@connect.ust.hk>
2023-04-22 01:35:01 +0800

    Update git log format

--- git.nix
+++ git.nix
@@ -6,8 +6,9 @@
     userEmail = "mhstsangab@connect.ust.hk";
     userName = "HinnyTsang";
     aliases = {
-      l =
-        "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
+      l = "log --graph --abbrev-commit --decorate --format=format:"
+        + "'%C(bold blue)%h%C(reset) - %C(bold cyan)%ah%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n'"
+        + "'          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
       tree = "ls-tree -r --name-only HEAD";
     };
   };
